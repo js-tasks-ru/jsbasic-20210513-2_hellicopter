0620751878f9c35559e9fe1a12e8d65e7bb26a08
Bonza <hellicopter@mail.ru>
2021-06-03 20:50:40 +0300

    Решение задач в Занятии 3

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,5 +1,5 @@
 function isEmpty(obj) {
-  for (let property in obj) {
+  for (let property in objпшgit) {
     return false;
   }
   return true;
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,17 @@
+const inputData = '1, -5.8 или 10, хотя 34 + -5.3 и 73';
+
+
 function getMinMax(str) {
-  // ваш код...
+  let minMax = str.split(' ');
+  minMax = minMax.map((number) => {
+    if (parseFloat(number))
+      return parseFloat(number);
+  })
+    .filter((number) => isFinite(number));
+
+
+  return {
+    min: minMax[0],
+    max: minMax[minMax.length - 1]
+  }
 }
