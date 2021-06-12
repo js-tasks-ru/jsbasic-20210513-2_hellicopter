9ff63f97f6f3025644eb0e1fc8921c8b63b58ee3
Bonza <hellicopter@mail.ru>
2021-06-12 15:10:15 +0300

    исправление в задании 5 занятия 3

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,17 +1,16 @@
 const inputData = '1, -5.8 или 10, хотя 34 + -5.3 и 73';
 
-
 function getMinMax(str) {
-  let minMax = str.split(' ');
-  minMax = minMax.map((number) => {
-    if (parseFloat(number))
-      return parseFloat(number);
-  })
-    .filter((number) => isFinite(number));
-
-
+  let stringWithoutSpaces = str.split(' ').join();
+  let itemsDividedByComma = stringWithoutSpaces.split(',');
+  let numbersOnly = itemsDividedByComma.filter((item) => item !== '' && isFinite(item));
+  let numbersInOrder = numbersOnly.map((value) => +value);
+  numbersInOrder.sort(function (a, b) {
+    return a - b
+  });
+  console.log(numbersInOrder)
   return {
-    min: minMax[0],
-    max: minMax[minMax.length - 1]
+    min: numbersInOrder[0],
+    max: numbersInOrder[numbersInOrder.length - 1]
   }
 }
