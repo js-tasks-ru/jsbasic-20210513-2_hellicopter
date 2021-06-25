13c7ce2f8eaae3105c6a84968b58a1a72a4a6dba
Bonza <hellicopter@mail.ru>
2021-06-25 15:22:52 +0300

    Решение задачи с каруселью

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,28 @@
 function initCarousel() {
-  // ваш код...
+  let carouselInner = document.querySelector('.carousel__inner')
+  let arrowRight = document.querySelector('.carousel__arrow_right');
+  let arrowLeft = document.querySelector('.carousel__arrow_left');
+  arrowLeft.style.display = 'none';
+  let slide = 0;
+  arrowRight.addEventListener('click', function () {
+    slide = slide + 988;
+    if (slide > -988) {
+      arrowLeft.style.display = '';
+    }
+    if (slide > 1976) {
+      arrowRight.style.display = 'none';
+    }
+    carouselInner.style.transform = `translateX(${-slide + 'px'})`;
+  })
+
+  arrowLeft.addEventListener('click', function () {
+    slide = slide - 988;
+    if (slide < 2964) {
+      arrowRight.style.display = '';
+    }
+     if (slide <= 0) {
+      arrowLeft.style.display = 'none';
+    }
+    carouselInner.style.transform = `translateX(${-slide + 'px'})`;
+  })
 }
