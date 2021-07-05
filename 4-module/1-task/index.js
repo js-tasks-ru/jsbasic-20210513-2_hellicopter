edc2d2c06afcb0b4b9642acec8310773b36f8961
Bonza <hellicopter@mail.ru>
2021-07-05 21:47:18 +0300

    исправление в задаче 1 Занятия 4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -15,7 +15,7 @@ let friends = [
 
 function makeFriendsList(friends) {
   let ul = document.createElement('ul')
-  let friendsList = friends.map(value => `<li>${value.firstName + " " + value.lastName}</li>`);
-  ul.append(friendsList);
+  let friendsList = friends.map(value => `<li>${value.firstName} ${value.lastName}</li>`);
+  ul.innerHTML = friendsList.join('');
   return ul
 }
